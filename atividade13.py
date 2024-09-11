de470a3382b6f63448b430fb4ae9548107ab98cf
Miguel-nextage <Pmiguelhkss@gmail.com>
2024-09-11 14:34:50 -0300

    Positivo ou Negativo

---
+++ atividade13.py
@@ -0,0 +1,12 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+
+Numb = int(input("Escreva seu numero aqui:"))
+
+if Numb > 0:
+    print("Seu numero é positivo")
+
+elif Numb == 0:
+    print("Seu numero é ZERO!")
+
+elif Numb < 0:
+    print("Seu numero é negativo")
